5e81ff0e4127c5d54bc615ae266b0be6718b3b70
clara seijo <clasei@proton.me>
2024-09-01 12:27:17 +0200

    solved lab

--- src/clue.js
+++ src/clue.js
@@ -4,152 +4,154 @@
 // Suspects Array
 const suspectsArray = [
   {
-    firstName: 'Jacob',
-    lastName: 'Green',
-    occupation: 'Entrepreneur',
+    firstName: "Jacob",
+    lastName: "Green",
+    occupation: "Entrepreneur",
     age: 45,
-    description: 'He has a lot of connections',
-    image: 'https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg',
-    color: 'green'
+    description: "He has a lot of connections",
+    image:
+      "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg",
+    color: "green",
   },
   {
-    firstName: 'Doctor',
-    lastName: 'Orchid',
-    occupation: 'Scientist',
+    firstName: "Doctor",
+    lastName: "Orchid",
+    occupation: "Scientist",
     age: 26,
-    description: 'PhD in plant toxicology. Adopted daughter of Mr. Boddy',
-    image: 'http://www.radiotimes.com/uploads/images/Original/111967.jpg',
-    color: 'white'
+    description: "PhD in plant toxicology. Adopted daughter of Mr. Boddy",
+    image: "http://www.radiotimes.com/uploads/images/Original/111967.jpg",
+    color: "white",
   },
   {
-    firstName: 'Victor',
-    lastName: 'Plum',
-    occupation: 'Designer',
+    firstName: "Victor",
+    lastName: "Plum",
+    occupation: "Designer",
     age: 22,
-    description: 'Billionaire video game designer',
-    image: 'https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg',
-    color: 'purple'
+    description: "Billionaire video game designer",
+    image:
+      "https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg",
+    color: "purple",
   },
   {
-    firstName: 'Kasandra',
-    lastName: 'Scarlet',
-    occupation: 'Actor',
+    firstName: "Kasandra",
+    lastName: "Scarlet",
+    occupation: "Actor",
     age: 31,
-    description: 'She is an A-list movie star with a dark past',
-    image: 'https://www.radiotimes.com/uploads/images/Original/111967.jpg',
-    color: 'red'
+    description: "She is an A-list movie star with a dark past",
+    image: "https://www.radiotimes.com/uploads/images/Original/111967.jpg",
+    color: "red",
   },
   {
-    firstName: 'Eleanor',
-    lastName: 'Peacock',
-    occupation: 'Socialité',
+    firstName: "Eleanor",
+    lastName: "Peacock",
+    occupation: "Socialité",
     age: 36,
-    description: 'She is from a wealthy family and uses her status and money to earn popularity',
-    image: 'https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg',
-    color: 'blue'
+    description:
+      "She is from a wealthy family and uses her status and money to earn popularity",
+    image: "https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg",
+    color: "blue",
   },
   {
-    firstName: 'Jack',
-    lastName: 'Mustard',
-    occupation: 'Retired Football player',
+    firstName: "Jack",
+    lastName: "Mustard",
+    occupation: "Retired Football player",
     age: 62,
-    description: 'He is a former football player who tries to get by on his former glory',
-    image: 'https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg',
-    color: 'yellow'
-  }
+    description:
+      "He is a former football player who tries to get by on his former glory",
+    image:
+      "https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg",
+    color: "yellow",
+  },
 ];
-
 
 // Weapons Array
 const weaponsArray = [
   {
     name: "rope",
-    weight: 10
+    weight: 10,
   },
   {
     name: "knife",
-    weight: 8
+    weight: 8,
   },
   {
     name: "candlestick",
-    weight: 2
+    weight: 2,
   },
   {
     name: "dumbbell",
-    weight: 30
+    weight: 30,
   },
   {
     name: "poison",
-    weight: 2
+    weight: 2,
   },
   {
     name: "axe",
-    weight: 15
+    weight: 15,
   },
   {
     name: "bat",
-    weight: 13
+    weight: 13,
   },
   {
     name: "trophy",
-    weight: 25
+    weight: 25,
   },
   {
     name: "pistol",
-    weight: 20
-  }
+    weight: 20,
+  },
 ];
-
 
 // Rooms Array
 const roomsArray = [
   {
-    name: "Dining Room"
+    name: "Dining Room",
   },
   {
-    name: "Conservatory"
+    name: "Conservatory",
   },
   {
-    name: "Kitchen"
+    name: "Kitchen",
   },
   {
-    name: "Study"
+    name: "Study",
   },
   {
-    name: "Library"
+    name: "Library",
   },
   {
-    name: "Billiard Room"
+    name: "Billiard Room",
   },
   {
-    name: "Lounge"
+    name: "Lounge",
   },
   {
-    name: "Ballroom"
+    name: "Ballroom",
   },
   {
-    name: "Hall"
+    name: "Hall",
   },
   {
-    name: "Spa"
+    name: "Spa",
   },
   {
-    name: "Living Room"
+    name: "Living Room",
   },
   {
-    name: "Observatory"
+    name: "Observatory",
   },
   {
-    name: "Theater"
+    name: "Theater",
   },
   {
-    name: "Guest House"
+    name: "Guest House",
   },
   {
-    name: "Patio"
-  }
+    name: "Patio",
+  },
 ];
-
 
 // **********
 // ITERATION 2
@@ -189,10 +191,9 @@ function pickMystery() {
 
 console.log(pickMystery());
 
-let mysteryTestOne = pickMystery()
-let mysteryTestTwo = pickMystery()
-let mysteryTestThree = pickMystery()
-
+let mysteryTestOne = pickMystery();
+let mysteryTestTwo = pickMystery();
+let mysteryTestThree = pickMystery();
 
 // **********
 // ITERATION 3
@@ -203,6 +204,8 @@ function revealMystery(envelope) {
   return `${envelope.suspect.firstName} ${envelope.suspect.lastName} killed Mr. Boddy using the ${envelope.weapon.name} in the ${envelope.room.name}!`;
 }
 
-console.log(revealMystery(mysteryTestOne))
-console.log(revealMystery(mysteryTestTwo))
-console.log(revealMystery(mysteryTestThree))
+console.log(revealMystery(mysteryTestOne));
+console.log(revealMystery(mysteryTestTwo));
+console.log(revealMystery(mysteryTestThree));
+
+console.log(`Here is your mistery: ${revealMystery(mysteryTestOne)} Happy finding 🔍`)
